641c731d2f9b47e54d7f0f3fa615e9eb1650b7c7
adarshgonela <adarshgonela@gmail.com>
2025-02-24 12:32:04 +0530

    changesss security

---
+++ spring-boot-spring-security-jwt-authentication-master/src/main/java/com/adarsh/springjwt/SpringBootSecurityJwtApplication.java
@@ -0,0 +1,15 @@
+package com.adarsh.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+@SpringBootApplication
+public class SpringBootSecurityJwtApplication {
+
+	public static void main(String[] args) {
+    SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+	}
+
+}
