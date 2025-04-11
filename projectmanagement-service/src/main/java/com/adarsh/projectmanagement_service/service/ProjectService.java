6691c4f60372dc7ca9487214b051919893e811f0
adarshgonela <adarshgonela@gmail.com>
2025-04-11 21:06:38 +0530

    creating project func done

--- projectmanagement-service/src/main/java/com/adarsh/projectmanagement_service/service/ProjectService.java
+++ projectmanagement-service/src/main/java/com/adarsh/projectmanagement_service/service/ProjectService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProjectService {
@@ -17,8 +16,8 @@ public class ProjectService {
     private Projectdao projectdao;
 
     public Project createproject(Project project) {
-        Optional<Project> optional = projectdao.getdatabyprojectname(project.getName());
-        if (!optional.isEmpty()) {
+        List<Project> optional = projectdao.getdatabyprojectname(project.getName());
+        if (optional.isEmpty()) {
             return projectdao.createproject(project);
         }
         throw new ProjectNotFoundException("the project you are entering is already present");
